c687ce1c627e09aec7aff0b3d0da50ee65724652
Samik Maharjan <samikmaharjan55@gmail.com>
2023-11-24 16:07:12 +0545

    Carousel Slider Model

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -8,4 +8,22 @@ class Category extends Equatable {
 
   @override
   List<Object?> get props => [name, imageUrl];
+
+  static List<Category> categories = [
+    const Category(
+      name: 'Soft Drink',
+      imageUrl:
+          'https://images.unsplash.com/photo-1551024709-8f23befc6f87?q=80&w=1857&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ),
+    const Category(
+      name: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1539574610665-dc92264e9c4f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ),
+    const Category(
+      name: 'Water',
+      imageUrl:
+          'https://images.unsplash.com/photo-1633949698015-0f8a8b261c07?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ),
+  ];
 }
