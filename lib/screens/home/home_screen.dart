df6da2d724a96db29a838a261ec0988c6a9c825e
Samik Maharjan <samikmaharjan55@gmail.com>
2023-11-24 16:13:46 +0545

    Hero Carousel Slider

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_app/models/category_model.dart';
-import 'package:ecommerce_app/widgets/custom_appbar.dart';
-import 'package:ecommerce_app/widgets/custom_navbar.dart';
 import 'package:flutter/material.dart';
+import '../../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = '/';
@@ -31,53 +30,6 @@ class HomeScreen extends StatelessWidget {
             .map((category) => HeroCarouselCard(category: category))
             .toList(),
       ),
-    );
-  }
-}
-
-class HeroCarouselCard extends StatelessWidget {
-  final Category category;
-  const HeroCarouselCard({super.key, required this.category});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(5.0),
-      child: ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-          child: Stack(
-            children: <Widget>[
-              Image.network(category.imageUrl,
-                  fit: BoxFit.cover, width: 1000.0),
-              Positioned(
-                bottom: 0.0,
-                left: 0.0,
-                right: 0.0,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color.fromARGB(200, 0, 0, 0),
-                        Color.fromARGB(0, 0, 0, 0)
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 10.0, horizontal: 20.0),
-                  child: Text(
-                    category.name,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )),
     );
   }
 }
