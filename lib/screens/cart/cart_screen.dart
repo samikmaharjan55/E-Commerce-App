2bf8ff0983c742a973fad6a2deb8247e7691203c
Samik Maharjan <samikmaharjan55@gmail.com>
2023-12-04 13:08:40 +0545

    Cart Screen Ongoing

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -2,6 +2,8 @@ import 'package:ecommerce_app/widgets/custom_appbar.dart';
 import 'package:ecommerce_app/widgets/custom_navbar.dart';
 import 'package:flutter/material.dart';
 
+import '../../models/models.dart';
+
 class CartScreen extends StatelessWidget {
   static const String routeName = '/cart';
   static Route route() {
@@ -15,9 +17,64 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Cart'),
-      bottomNavigationBar: CustomNavBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Cart'),
+      bottomNavigationBar: const CustomNavBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Add \$20.0 for FREE Delivery',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(),
+                    elevation: 0.0,
+                  ),
+                  child: Text(
+                    'Add More Items',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            CartProductCard(
+              product: Product.products[0],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartProductCard extends StatelessWidget {
+  final Product product;
+  const CartProductCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.network(
+          product.imageUrl,
+          width: 100,
+          height: 80,
+          fit: BoxFit.cover,
+        ),
+      ],
     );
   }
 }
